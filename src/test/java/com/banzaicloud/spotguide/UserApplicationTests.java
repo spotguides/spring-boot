d206db8874b69542776fe204c71ec3f4d9569077
Nandor Kracser <bonifaido@gmail.com>
2018-11-07 16:27:04 +0100

    Use H2 in-memory database for test

--- src/test/java/com/banzaicloud/spotguide/UserApplicationTests.java
+++ src/test/java/com/banzaicloud/spotguide/UserApplicationTests.java
@@ -1,4 +1,4 @@
-package com.banzaicloud.vaultspotguide;
+package com.banzaicloud.spotguide;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,11 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class VaultApplicationTests {
+@SpringBootTest(properties = "spring.datasource.url=jdbc:h2:mem:testdb")
+public class UserApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
