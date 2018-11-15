2b5bec2a2469bc1ec36df1183fb75990ad630b6c
Nandor Kracser <bonifaido@gmail.com>
2018-11-15 11:41:57 +0100

    Use spring-boot-devtools for hot-reload (IDEA Build project has to be pushed)

--- src/main/java/com/banzaicloud/spotguide/UserController.java
+++ src/main/java/com/banzaicloud/spotguide/UserController.java
@@ -9,14 +9,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "User not found")
 class UserNotFoundException extends RuntimeException {
 }
 
 @RestController
+@RequestMapping("/api/v1")
 public class UserController {
 
     @Autowired
